e27c8372ca9895873cdb6c487de6098de73616ac
KineBergseth <kine96@hotmail.com>
2021-11-01 15:07:44 +0100

    Entschuldigung

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,6 @@ import Queue from "./pages/queue";
 import Summary from "./pages/summary";
 
 function App() {
-    /*const [data, setData] = React.useState(null);
-
-    React.useEffect(() => {
-        fetch("/api")
-            .then((res) => res.json())
-            .then((data) => setData(data.message));
-    }, []);*/
 
     return (<>
             <div className="App">
